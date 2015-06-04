be9e5c887cff7900dfb3378dac6d7011180b7699
Valentin <valentindu64@hotmail.fr>
2015-06-04 12:06:03 +0200

    Affichage graphique ok

--- thermostat.plugin.enabled.php
+++ thermostat.plugin.enabled.php
@@ -10,6 +10,11 @@
 
 //Si vous utiliser la base de donnees a ajouter
 //include('thermostat.class.php');
+Plugin::addCss("/yana-server/plugins/thermostat/css/style.css"); 
+Plugin::addJs("/yana-server/plugins/thermostat/js/highstock.js"); 
+Plugin::addJs("/yana-server/plugins/thermostat/js/highcharts.js"); 
+Plugin::addJs("/yana-server/plugins/thermostat/js/exporting.js"); 
+Plugin::addJs("/yana-server/plugins/thermostat/js/main.js"); 
 
 //Cette fonction va generer un nouveau element dans le menu
 function test_plugin_menu(&$menuItems){
@@ -20,10 +25,10 @@ function test_plugin_menu(&$menuItems){
 //Cette fonction ajoute une commande vocale
 function test_plugin_vocal_command(&$response,$actionUrl){
 	global $conf;
-	//Création de la commande vocale "Yana, commande de test" avec une sensibilité de 0.90 et un appel 
+	//Création de la commande vocale "Yana, temperature de la chambre" avec une sensibilité de 0.90 et un appel 
 	// vers l'url /action.php?action=thermostat_plugin_vocal_test après compréhension de la commande
 	$response['commands'][] = array(
-		'command'=>$conf->get('VOCAL_ENTITY_NAME').' commande vocale de test',
+		'command'=>$conf->get('VOCAL_ENTITY_NAME').', temperature de la chambre',
 		'url'=>$actionUrl.'?action=test_plugin_vocal_test','confidence'=>('0.90'+$conf->get('VOCAL_SENSITIVITY'))
 		);
 }
@@ -31,12 +36,12 @@ function test_plugin_vocal_command(&$response,$actionUrl){
 //cette fonction comprends toutes les actions du plugin qui ne nécessitent pas de vue html
 function test_plugin_action(){
 	global $_,$conf;
-	$temperatureGet = (integer) temperatureGet();
-	//Action de réponse à la commande vocale "Yana, commande de test"
+	$temperatureGet = temperatureGet();
+	//Action de réponse à la commande vocale "Yana, temperature de la chambre"
 	switch($_['action']){
 		case 'test_plugin_vocal_test':
 			$response = array('responses'=>array(
-										array('type'=>'talk','sentence'=>'La température est de .$temperatureGet. °C')
+										array('type'=>'talk','sentence'=>'La température est de ' .$temperatureGet. '°C')
 											)
 								);
 			$json = json_encode($response);
@@ -45,27 +50,83 @@ function test_plugin_action(){
 	}
 }
 
-function resultat_database(){
-        // connexion a la base
-        $base = mysql_connect('localhost', 'userdist', '123456');
-        mysql_select_db('temperature',$base);
-        
-// lancement de la requete
-        $sql = 'SELECT * FROM Temperature WHERE 1';
-        $req = mysql_query($sql) or die ('Erreur SQL ! <br />' .$sql.'<br />'.mysql_error());
-	while ($row = mysql_fetch_array($req)){
-		//traiter les données
+function get_max(){
+	$req= connection_database();
+        mysql_data_seek($req, mysql_num_rows($req) - 337);
+	$max = -150;
+	while($row = mysql_fetch_array($req)){
+		if ($row[3] > $max){
+			$max = $row[3];
+		}
 	}
 
-
-//  on libère la place mémoire allouée pour l'interrogation de la base
-        mysql_free_result($req);
-        mysql_close();
+mysql_free_result($req);
+$max = substr($max, 0, -1);
+return $max;
 }
+function get_min(){
+        $req = connection_database();
+        mysql_data_seek($req, mysql_num_rows($req) - 337);
+        $min = +150;
+        while($row = mysql_fetch_array($req)){
+                if ($row[3] < $min){
+                        $min = $row[3];
+                }
+        }
+mysql_free_result($req);
+$min = substr($min, 0, -1);
+
+return $min;
+}
+
+function get_average(){
+        $req = connection_database();
+	mysql_data_seek($req, mysql_num_rows($req) - 337);
+	$average=0;
+	$compteur = 0;
+        while ($row = mysql_fetch_array($req)){
+		$average += $row[3];
+		$compteur +=1;
+	}
+$average = substr($average / $compteur , 0, 5);
+mysql_free_result($req);
+$average = substr($average, 0, -1);
+
+return $average;
+}
+function connection_database(){
+// connexion a la base
+        $base = mysql_connect('localhost', 'userdist', '123456');
+        mysql_select_db('temperature',$base);
+
+// lancement de la requete
+        $sql = 'SELECT * FROM Temperature';
+        $req = mysql_query($sql) or die ('Erreur SQL ! <br />' .$sql.'<br />'.mysql_error());
+return $req;
+}
+
+function resultat_database(){
+        $req = connection_database();
+	mysql_data_seek($req, mysql_num_rows($req) - 337);
+	$data='';
+	$compteur = 0;
+	while ($row = mysql_fetch_array($req) and $compteur <337){
+		//traiter les données
+		// on récupère l'heure et la valeur de la temperature
+		$data .= ('[\'' .substr($row[1],5). ' ' .substr($row[2],0,-3). '\','.$row[3].'],');
+		$compteur += 1;
+	}
+$data = substr($data, 0, -1);
+mysql_free_result($req);
+
+return $data;
+}
+
 
 //Cette fonction va generer une page quand on clique sur Thermostat dans menu
 function test_plugin_page($_){
 	if(isset($_['module']) && $_['module']=='test'){
+
 	?>
 	<div class="span9">
 
@@ -80,33 +141,32 @@ function test_plugin_page($_){
 	      <li class="active"><a href="#">Etat du chauffage</a></li>
 	      <li><a href="#">Programmation</a></li>
 	    </ul>
-
+<!--
 	    <h2>Barre de progression</h2>
             <div class="progress progress-striped active">
             <div class="bar" style="width: 100%;"></div>
             </div>
-
+-->
 
 	    <h2>Résumé</h2>
 	    <table class="table table-striped table-bordered table-hover">
 	    <thead>
 	    <tr>
 	    <th>Température Actuelle</th>
+	    <th>Température moyenne (7 jours)</th>
+	    <th>Température la plus haute (7jours)</th>
+            <th>Température la plus basse (7jours)</th>
 	    </tr>
 	    </thead>
-	    <tr><td><?php echo temperatureGet();?>°C</td></tr>
+	    <tr>
+	    <td><?php echo temperatureGet();?>°C</td>
+	    <td><?php echo get_average();?>°C</td>
+	    <td><?php echo get_max();?>°C</td>
+	    <td><?php echo get_min();?>°C</td></tr>
 	    </table>
-	 <h2> Affichage des résultats</h2>
-	<div id="container" style="height: 400px; min-width: 310px"></div>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-	<script src="http://code.highcharts.com/highcharts.js"></script>
-	<script src="http://code.highcharts.com/stock/highstock.js"></script>
-	<script src="http://code.highcharts.com/stock/modules/exporting.js"></script>
-	<script src="js/main.js"></script>
-	<script src="js/gray.js"></script>
-	<?php resultat_database();?>
-
-
+	 <h2> Affichage des résultats des 7 derniers jours</h2>
+  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<div id="chart_div" style="width:1000; height:500"></div>
 	    <h2>Référence</h2>
 	    <div class="pagination">
 	    <ul>
@@ -117,7 +177,40 @@ function test_plugin_page($_){
 	    </div>
 
 	</div>
-<?php
+
+ <script type="text/javascript">
+google.load('visualization', '1', {packages: ['corechart', 'line']});
+google.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Date - Heure');
+      data.addColumn('number', 'Temperature');
+
+      data.addRows([
+		<?php echo resultat_database();?>
+      ]);
+
+      var options = {
+crosshair: { trigger: 'both' },
+'width':1000,
+                     'height':500,
+        hAxis: {
+          title: 'Heure'
+        },
+        vAxis: {
+          title: 'Temperatre (°C)'
+        }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+      chart.draw(data, options);
+    }
+
+</script>
+<?php mysql_close();
 	}
 }
 
@@ -128,7 +221,7 @@ function temperatureGet(){
                                 $filename = "/sys/bus/w1/devices/".$entry."/w1_slave" ;
                                 if (file_exists($filename)) {
                                         $lines = file($filename);
-                                        $currenttemp = round ( substr($lines[1], strpos($lines[1], "t=")+2) / 1000 , 1) ;
+                                        $currenttemp = round ( substr($lines[1], strpos($lines[1], "t=")+2) / 1000 , 2) ;
                                         closedir($handle);
                                         return $currenttemp;
                                 }
@@ -138,16 +231,7 @@ function temperatureGet(){
         }
         return "N/A";
 }
-
-
-
-Plugin::addCss("/css/style.css"); 
-Plugin::addJs("/js/jquery.js"); 
-Plugin::addJs("/js/highstock.js"); 
-Plugin::addJs("/js/highcharts.js"); 
-//Plugin::addJs("/js/highmaps.js"); 
-//Plugin::addJs("/js/exporting.js"); 
-//Plugin::addJs("/js/main.js"); 
+ 
 
 Plugin::addHook("menubar_pre_home", "test_plugin_menu");  
 Plugin::addHook("home", "test_plugin_page");  
